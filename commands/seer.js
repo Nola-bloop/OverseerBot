8de51beea790b1e9aacfe173e18bf1af1a656f83
Nola <Nola.Bloop@gmail.com>
2026-01-31 20:46:09 -0500

    fast commit

--- commands/seer.js
+++ commands/seer.js
@@ -1,5 +1,11 @@
 import { SlashCommandBuilder, MessageFlags } from 'discord.js';
 import caller from '../API-calls.js';
+
+const AUTHORIZED_USERS = [
+	"300012833016512514", //Nola
+	"1290040130622591038",
+]
+
 export default {
 	data: new SlashCommandBuilder()
 	.setName('dnd')
@@ -24,6 +30,21 @@ export default {
         		.setDescription('The name of the chapter group.')
         		.setRequired(true)
         )
+	)
+	.addSubcommand(subCommand =>
+		addSubcommand
+		.setName('track-channel')
+		.setDescription('Add this channel to the tracked channel list.')
+	)
+	.addSubcommand(subCommand =>
+		addSubcommand
+		.setName('forced-refresh')
+		.setDescription('Make the bot scan the channels ahead of schedule.')
+	)
+	.addSubcommand(subCommand =>
+		addSubcommand
+		.setName('url')
+		.setDescription("Get a link to the guild's campaign.")
 	),
 	async execute (interaction) {
 		const user = interaction.member.user;
@@ -32,9 +53,12 @@ export default {
 		const channel = interaction.channel
 
 		if (sub === "passwd"){
+			return await caller.Reply(interaction, "Command not implemented yet.")
 			caller.UpdatePassword(user.id, user.username, interaction.options.getString("p"))
 		}
 		else if (sub === "set-group") {
+			return await caller.Reply(interaction, "Command not implemented yet.")
+
 			let campaign = caller.GetGuildCampaign(guild.id)
 			if (campaign.response){ console.log(campaign.response); caller.Reply(interaction, "Could not create the chapter group: "+campaign.response) }
 			let name = interaction.options.getString("n")
@@ -48,6 +72,30 @@ export default {
 			//chapter group should exist, provided the API works.
 
 			await caller.UpdateChapterToGroupRelation(chapterGroup.id, chapter.id)
+		}
+		else if (sub === "track-channel"){
+			if (!AUTHORIZED_USERS.contains(user.id)){
+				return await caller.Reply(interaction, "You are not authorized to use this command.")
+			}
+
+			let campaign = await caller.GetGuildCampaign(guild.id)
+			if (campaign.response){ console.log(campaign.response); return await caller.Reply(message, "Could not create the chapter: "+campaign.response) }
+			if (!campaign){ console.log("Could not fetch campaign"); return await caller.Reply(message, "Could not create the chapter.") }
+
+			await caller.CreateChapter(channel.name, 1, channel.id, campaign.id, 0)
+			await caller.Reply(message, "Success.")
+
+		}
+		else if (sub === "forced-refresh"){
+			if (!AUTHORIZED_USERS.contains(user.id)){
+				return await caller.Reply(interaction, "You are not authorized to use this command.")
+			}
+
+			await caller.LogNewMessages(client)
+			return await caller.Respond(message, "success")
+		}
+		else if (sub === "url"){
+			return await caller.Respond(message, "[nolar-eclipse.ca](https://nolar-eclipse.ca/?guild="+guild.id+")")
 		}
 	}
 };
