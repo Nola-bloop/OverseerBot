8e612fd41049fbf9d3ad0643fbe78aba4d4b56b8
Nola <Nola.Bloop@gmail.com>
2026-04-06 21:12:07 -0400

    fast commit

--- entries.js
+++ entries.js
@@ -7,15 +7,14 @@ test entry.
 Entries will contain a brief description of the enemy, possibly special encounters as well.
 
 There can also be a record of usual stats for this creature, like
-\`\`\`
-hp : 20-25
-str: 1
-dex: -2
-con: 1
-int: -5
-wis: -5
-cha: -5
-\`\`\`
+
+> hp : 20-25
+> str: 1
+> dex: -2
+> con: 1
+> int: -5
+> wis: -5
+> cha: -5
 
 *!! Inflicts necrosis on melee hit.*
 
